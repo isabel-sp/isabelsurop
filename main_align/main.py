4a57265e235a18e29cc0f57c18124a512f811d5d
Isabel Sperandio <isabel.sperandio@gmail.com>
2021-07-27 15:10:10 -0400

    big code reformat

---
+++ main_align/main.py
@@ -0,0 +1,16 @@
+'''
+Main file to run to align
+'''
+
+#IMPORTS
+import cv2
+import cam_helper
+import piezo_helper
+import img_process
+import display_callback
+
+#OPEN WINDOWS
+
+
+
+#CLOSE WINDOWS
--- test.py
+++ test.py
@@ -1,9 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import cv2
 
 
-x = np.random.randint(0,10,5)
-y = np.random.randint(0,10,5)
-    
-plt.plot(x, y, '-bD',  c='blue', mfc='red', mec='k')
-plt.show()
+img_raw = cv2.imread('3clicked.png')
+img_raw = cv2.resize(img_raw, (1440, 1080))
+cv2.imwrite('test_14401080.png', img_raw)
